bc547e718fdb7c9b5e9321884a9b01b4ce5d90a4
Ranjith <ranjiththarayil@gmail.com>
2023-11-17 21:01:04 +0530

    init checkin

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # csharp-exmaple
+
+
+
+https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
+
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-8.0
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-8.0
+
+  https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-8-0
---
+++ unit-testing-using-mstest/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
